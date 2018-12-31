a55204d8da0015e1eb21e8aef97b6d0789a9dd7e
Gordon Lau Wai chung <gordon@tecky.io>
2018-12-31 15:54:41 +0800

    Updated the constants to avoid linkstarts with http

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 exports.METHODS = { GET: "GET", POST: "POST" };
 exports.MARKDOWN_IMAGE_REGEXP_GLOBAL = /!\[[^\]]*\]\(([^\)]*)\)/g;
-exports.MARKDOWN_ASSET_REGEXP_GLOBAL = /[^!]\[[^\]]*\]\(([^\)]*\.[a-zA-Z]{2,4})\)/g;
+exports.MARKDOWN_ASSET_REGEXP_GLOBAL = /[^!]\[[^\]]*\]\((?!http)([^\)]*)\)/g;
 exports.MARKDOWN_IMAGE_REGEXP = /!\[[^\]]*\]\(([^\)]*)\)/;
-exports.MARKDOWN_ASSET_REGEXP = /[^!]\[[^\]]*\]\(([^\)]*)\)/;
+exports.MARKDOWN_ASSET_REGEXP = /[^!]\[[^\]]*\]\((?!http)([^\)]*)\)/;
