7dfad2ef3713c074303d0a793eb0adc0d419d545
WhippetsAintDogs <marc.antoine.fortier.94@gmail.com>
2019-01-21 23:19:10 -0500

    chore(Linter): Add prettier to the project

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,60 +1,60 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs')
+const path = require('path')
 
-const CockpitService = require("./src/CockpitService");
-const CollectionItemNodeFactory = require("./src/CollectionItemNodeFactory");
+const CockpitService = require('./src/CockpitService')
+const CollectionItemNodeFactory = require('./src/CollectionItemNodeFactory')
 const {
   MARKDOWN_IMAGE_REGEXP_GLOBAL,
-  MARKDOWN_ASSET_REGEXP_GLOBAL
-} = require("./src/constants");
-const FileNodeFactory = require("./src/FileNodeFactory");
-const MarkdownNodeFactory = require("./src/MarkdownNodeFactory");
+  MARKDOWN_ASSET_REGEXP_GLOBAL,
+} = require('./src/constants')
+const FileNodeFactory = require('./src/FileNodeFactory')
+const MarkdownNodeFactory = require('./src/MarkdownNodeFactory')
 
 exports.sourceNodes = async ({ actions, cache, store }, configOptions) => {
-  const { createNode } = actions;
+  const { createNode } = actions
   const cockpit = new CockpitService(
     configOptions.baseUrl,
     configOptions.token,
     configOptions.locales
-  );
-  const fileNodeFactory = new FileNodeFactory(createNode, store, cache);
-  const markdownNodeFactory = new MarkdownNodeFactory(createNode);
+  )
+  const fileNodeFactory = new FileNodeFactory(createNode, store, cache)
+  const markdownNodeFactory = new MarkdownNodeFactory(createNode)
 
-  await cockpit.validateBaseUrl();
-  await cockpit.validateToken();
+  await cockpit.validateBaseUrl()
+  await cockpit.validateToken()
 
-  const collections = await cockpit.getCollections();
-  const images = await cockpit.normalizeCollectionsImages(collections);
-  const assets = await cockpit.normalizeCollectionsAssets(collections);
+  const collections = await cockpit.getCollections()
+  const images = await cockpit.normalizeCollectionsImages(collections)
+  const assets = await cockpit.normalizeCollectionsAssets(collections)
   const markdowns = await cockpit.normalizeCollectionsMarkdowns(
     collections,
     images,
     assets
-  );
+  )
 
   for (let path in images) {
-    const imageNode = await fileNodeFactory.createImageNode(path);
+    const imageNode = await fileNodeFactory.createImageNode(path)
     images[path] = {
       localPath: copyFileToStaticFolder(imageNode),
-      id: imageNode.id
-    };
+      id: imageNode.id,
+    }
   }
 
   for (let path in assets) {
-    const assetNode = await fileNodeFactory.createAssetNode(path);
+    const assetNode = await fileNodeFactory.createAssetNode(path)
     assets[path] = {
       localPath: copyFileToStaticFolder(assetNode),
-      id: assetNode.id
-    };
+      id: assetNode.id,
+    }
   }
 
   for (let markdown in markdowns) {
     const localMarkdown = updateAssetPathsWithLocalPaths(
       updateImagePathsWithLocalPaths(markdown, images),
       assets
-    );
-    const id = markdownNodeFactory.create(localMarkdown);
-    markdowns[markdown] = { id };
+    )
+    const id = markdownNodeFactory.create(localMarkdown)
+    markdowns[markdown] = { id }
   }
 
   collections.forEach(collection => {
@@ -64,36 +64,36 @@ exports.sourceNodes = async ({ actions, cache, store }, configOptions) => {
       images,
       assets,
       markdowns
-    );
+    )
 
     collection.items.forEach(item => {
-      nodeFactory.create(item);
-    });
-  });
-};
+      nodeFactory.create(item)
+    })
+  })
+}
 
 const copyFileToStaticFolder = ({ absolutePath, name, ext, internal }) => {
   const localPath = path.join(
-    "/",
-    "static",
+    '/',
+    'static',
     `${name}-${internal.contentDigest}${ext}`
-  );
+  )
 
-  fs.copyFileSync(absolutePath, path.join(process.cwd(), "public", localPath));
+  fs.copyFileSync(absolutePath, path.join(process.cwd(), 'public', localPath))
 
-  return localPath;
-};
+  return localPath
+}
 
 const updateImagePathsWithLocalPaths = (markdown, images) => {
   return markdown.replace(MARKDOWN_IMAGE_REGEXP_GLOBAL, (...match) =>
     match[0].replace(match[1], images[match[1]].localPath)
-  );
-};
+  )
+}
 
 const updateAssetPathsWithLocalPaths = (markdown, assets) => {
   return markdown.replace(MARKDOWN_ASSET_REGEXP_GLOBAL, (...match) =>
     assets[match[1]]
       ? match[0].replace(match[1], assets[match[1]].localPath)
       : match[0]
-  );
-};
+  )
+}
