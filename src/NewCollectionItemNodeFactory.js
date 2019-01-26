544daf13da9ac4e3ed780601496b539f14b3ded3
Markus Oehler <markus@oehler.at>
2019-01-26 11:53:21 +0100

    - Fixed merge error  - Removed console.log statement

--- src/NewCollectionItemNodeFactory.js
+++ src/NewCollectionItemNodeFactory.js
@@ -1,17 +1,16 @@
-const colorString = require('color-string');
+const colorString = require('color-string')
 
 const {
   createNodeFactory,
-  generateNodeId
-} = require("gatsby-node-helpers").default({
-  typePrefix: "Cockpit"
-});
+  generateNodeId,
+} = require('gatsby-node-helpers').default({
+  typePrefix: 'Cockpit',
+})
 
 module.exports = class NewCollectionItemNodeFactory {
-
   constructor(createNode, collectionName, images, assets, markdowns, config) {
-    this.createNode = createNode;
-    this.collectionName = collectionName;
+    this.createNode = createNode
+    this.collectionName = collectionName
     this.resources = {
       collectionName,
       images,
@@ -24,124 +23,123 @@ module.exports = class NewCollectionItemNodeFactory {
   create(collectionItem) {
     const children = collectionItem.hasOwnProperty('children')
       ? collectionItem.children.map(childItem => {
-        return this.create(childItem);
-      })
-      : [];
-    delete collectionItem.children;
+          return this.create(childItem)
+        })
+      : []
+    delete collectionItem.children
 
     // TODO: check if this is required - nodeFactory should create a node id already
-    const generateNodeIdFromItem = (item) => {
+    const generateNodeIdFromItem = item => {
       return generateNodeId(
         this.collectionName,
-        item.lang === "any"
-          ? item.cockpitId
-          : `${item.cockpitId}_${item.lang}`
+        item.lang === 'any' ? item.cockpitId : `${item.cockpitId}_${item.lang}`
       )
     }
 
     const CollectionItem = createNodeFactory(this.collectionName, node => {
-      node.id = generateNodeIdFromItem(node);
+      node.id = generateNodeIdFromItem(node)
 
       if (Array.isArray(node.children)) {
         node.children___NODE = node.children.map(child => {
-          child.parent___NODE = node.id;
-          return child.id;
-        });
-        delete node.children;
+          child.parent___NODE = node.id
+          return child.id
+        })
+        delete node.children
       }
 
-      return node;
-    });
+      return node
+    })
 
     const node = CollectionItem({
-        ...generateNodeData(collectionItem, this.resources),
-        children
-      }
-    );
-    this.createNode(node);
-    return node;
+      ...generateNodeData(collectionItem, this.resources),
+      children,
+    })
+    this.createNode(node)
+    return node
   }
 }
 
 const generateNodeData = (item, resources) => {
-  const result = {};
+  const result = {}
   // TODO: change this to a reduce implementation
   Object.keys(item).forEach(name => {
     if (['cockpitId', 'lang', 'level'].includes(name)) {
-      result[name] = item[name];
+      result[name] = item[name]
     } else if (['children', 'parent'].includes(name)) {
       // do nothing as we handle this values later in the process
     } else {
       const fieldData = {
         ...item[name],
         name,
-      };
+      }
       const [newName, newValue] = processField(fieldData, {
         ...resources,
         item,
-      });
-      result[newName] = newValue;
+      })
+      result[newName] = newValue
     }
-  });
-  return result;
+  })
+  return result
 }
 
 const processField = (fieldData, resources) => {
-  const { name, type } = fieldData;
+  const { name, type } = fieldData
 
   if (!valueTransformers.hasOwnProperty(type)) {
-    console.warn(`Unknown field type '${type}' found for field '${name}' - skipping field.`);
-    return [name, null];
+    console.warn(
+      `Unknown field type '${type}' found for field '${name}' - skipping field.`
+    )
+    return [name, null]
   }
 
-  return valueTransformers[type](fieldData, resources);
+  return valueTransformers[type](fieldData, resources)
 }
 
 const transformScalarFieldValue = ({ name, value }) => {
-  return [name, value];
+  return [name, value]
 }
 
 const transformAssetFieldValue = ({ name, value }, { assets }) => {
   if (assets.hasOwnProperty(value)) {
-    return [`${name}___NODE`, assets[value].id];
+    return [`${name}___NODE`, assets[value].id]
   }
-  return [name, null];
+  return [name, null]
 }
 
 const transformImageFieldValue = ({ name, value }, { images }) => {
   if (images.hasOwnProperty(value)) {
-    return [`${name}___NODE`, images[value].id];
+    return [`${name}___NODE`, images[value].id]
   }
-  return [name, null];
+  return [name, null]
 }
 
 const transformGalleryFieldValue = ({ name, value }, { images }) => {
   if (Array.isArray(value)) {
-    const result = value.map(({ value: imageUrl }) =>
-      images.hasOwnProperty(imageUrl)
-        ? images[imageUrl].id
-        : null
-    ).filter(image => image !== null);
-    return [`${name}___NODE`, result];
+    const result = value
+      .map(({ value: imageUrl }) =>
+        images.hasOwnProperty(imageUrl) ? images[imageUrl].id : null
+      )
+      .filter(image => image !== null)
+    return [`${name}___NODE`, result]
   }
 
-  return [fieldName, []];
+  return [fieldName, []]
 }
 
 const transformColorFieldValue = ({ name, value }) => {
-  const parsedColor = colorString.get.rgb(value);
-  const [ red, green, blue, alpha ] = parsedColor;
+  const parsedColor = colorString.get.rgb(value)
+  const [red, green, blue, alpha] = parsedColor
   const parsedValue = {
     parsed: {
       red,
       green,
       blue,
-      alpha
+      alpha,
     },
     hex: colorString.to.hex(parsedColor),
     rgb: colorString.to.rgb(parsedColor),
-  };
-  return [name, parsedValue];
+  }
+  return [name, parsedValue]
 }
 
 const transformHTMLFieldValue = ({ name, value }) => {
@@ -149,74 +147,72 @@ const transformHTMLFieldValue = ({ name, value }) => {
   //  - parse HTML Code into react representation
   //  - validate html code and strip stuff we don't want
   //  - replace image URLS
-  return [name, value];
+  return [name, value]
 }
 
 const transformMarkdownFieldValue = ({ name, value }, { markdowns }) => {
-  return [`${name}___NODE`, markdowns[value].id];
-};
+  return [`${name}___NODE`, markdowns[value].id]
+}
 
 const transformLayoutFieldValue = ({ name, value }) => {
   // TODO:
   //  - create separate node for layout object
   //  - parse data in add-entity-fields.js
-  return [name, JSON.stringify(value)];
+  return [name, JSON.stringify(value)]
 }
 
 const transformObjectFieldValue = ({ name, value }) => {
   // TODO:
   //  - create separate node for json object
   //  - parse data in add-entity-fields.js
-  return [name, JSON.stringify(value)];
+  return [name, JSON.stringify(value)]
 }
 
 const transformUnsupportedTypeFiledValue = ({ name, value, type }) => {
-  console.warn(`Field type '${type}' is currently not supported by @fika/gatsby-source-cockpit`);
-  return [name, null];
+  console.warn(
+    `Field type '${type}' is currently not supported by @fika/gatsby-source-cockpit`
+  )
+  return [name, null]
 }
 
 const transformSetFieldValue = ({ name, value }, resources) => {
-  return [name, null];
+  return [name, null]
 }
 
 const transformRepeaterFieldValue = ({ name, value }, resources) => {
   if (Array.isArray()) {
-    return [name, null];
+    return [name, null]
   }
 }
 
 const transformCollectionLinkFieldValue = ({ name, value }, { item }) => {
-
   // TODO: we should probably "normalize" the collection link ids much earlier in the process
-  console.log(value);
   if (Array.isArray(value)) {
-    const collectionName = value[0].link;
+    const collectionName = value[0].link
 
     value.forEach(linkedCollection => {
       if (linkedCollection.link !== collectionName) {
         throw new Error(
           `One to many Collection-Links must refer to entries from a single collection (concerned field: ${fieldName})`
-        );
+        )
       }
-    });
+    })
 
     const result = value.map(linkedCollection =>
       generateNodeId(
         linkedCollection.link,
-        item.lang === "any"
+        item.lang === 'any'
           ? linkedCollection._id
           : `${linkedCollection._id}_${item.lang}`
       )
-    );
-    return [`${name}___NODE`, result];
+    )
+    return [`${name}___NODE`, result]
   } else {
     const linkedNodeId = generateNodeId(
       value.link,
-      item.lang === "any"
-        ? value._id
-        : `${value._id}_${item.lang}`
-    );
-    return [`${name}___NODE`, linkedNodeId];
+      item.lang === 'any' ? value._id : `${value._id}_${item.lang}`
+    )
+    return [`${name}___NODE`, linkedNodeId]
   }
 }
 
@@ -251,7 +247,6 @@ const valueTransformers = {
   // nested fields need to be processed recursively
   collectionlink: transformCollectionLinkFieldValue,
 
-
   // JSON data is stored stringified (and later stored parsed in a GraphQLJSON field)
   object: transformObjectFieldValue,
   layout: transformLayoutFieldValue,
@@ -260,7 +255,7 @@ const valueTransformers = {
   // not implemented yet but should be possible to implement
   'access-list': transformUnsupportedTypeFiledValue,
   'account-link': transformUnsupportedTypeFiledValue,
-  'file': transformUnsupportedTypeFiledValue,
+  file: transformUnsupportedTypeFiledValue,
 
   // password is returned encoded; there is probably no situation where this will
   // be useful in Gatsby therefore we just print a warning and ignore it for now.
