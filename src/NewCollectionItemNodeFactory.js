64c7cf5945f8fc97845452d6ff9eb82aca0a4187
Markus Oehler <markus@oehler.at>
2019-01-26 09:08:10 +0100

    Some code cleanup and refactoring

--- src/NewCollectionItemNodeFactory.js
+++ src/NewCollectionItemNodeFactory.js
@@ -65,124 +65,55 @@ module.exports = class NewCollectionItemNodeFactory {
   /**
    *
    * @param fieldName
-   * @param type
-   * @param value
+   * @param fieldData
    * @returns [ string, mixed] tuple of new field name and new field value
    */
-  processField(fieldName, { type, value }) {
+  processField(fieldName, fieldData) {
+    const { type } = fieldData;
     // switch type, assign value
     if (!valueTransformers.hasOwnProperty(type)) {
       console.warn(`Unknown field type '${type}' found for field '${fieldName}' - skipping field.`);
       return [fieldName, null];
     }
-    return valueTransformers[type](fieldName, value, this.resources, type);
-    /*
-        switch (type) {
-
-          case 'multipleselect':
-            // TODO: check if we need to do something (e.g. split values, ...)
-            return [fieldName, value];
-
-          case 'date':
-            // TODO: check if we can parse it or store it in some other way than just a string
-            return [fieldName, value];
-
-          case 'access-list': case 'account-link':
-            console.warn(`Field type '${type}' is currently not supported by @fika/gatsby-source-cockpit`);
-            return [fieldName, null];
-
-          case 'password':
-            // I cannot think of a use case for sending the password to gatsby so I am not implementing this for now.
-            console.warn(`Field type '${type}' is currently not supported by @fika/gatsby-source-cockpit`);
-            return [fieldName, null];
-
-          case 'asset':
-            return this.processAssetField(fieldName, value);
-
-          case 'file':
-            // TODO: deal with file
-            return [fieldName, null];
-
-          case 'image':
-            return this.processImageField(fieldName, value);
-
-          case 'gallery':
-            // TODO: deal with gallery
-            return [fieldName, null];
-
-          case 'layout': case 'layout-grid':
-            // TODO: deal with layout & layout-grid
-            return [fieldName, null];
-
-          case 'object':
-            // TODO: deal with object
-            return [fieldName, null];
-
-          case 'html': case 'wysiwyg': case 'textarea':
-            // TODO: parse HTML, Strip Tags, ... (See handling layout fields for more info)
-            return [fieldName, value];
-
-          case 'markdown':
-            // TODO: implement markdown field
-            return [fieldName, value];
-
-          case 'collectionlink':
-            // TODO: deal with collectionlinks
-            return [fieldName, null];
-
-          case 'repeater':
-            // TODO: Implement support for repeater
-            return [fieldName, []];
-
-          case 'tags':
-            // TODO: Implement support for tags
-            return [fieldName, []];
-
-          case 'set':
-            // TODO: Implement support for set
-            return [fieldName, {}];
-
-          default:
-            console.warn(`Unknown field type '${type}' found - skipping field.`);
-            return [fieldName, null];
-        }
-      }
-    */
+    return valueTransformers[type]({
+      ...fieldData,
+      name: fieldName,
+    }, this.resources);
   }
 }
 
-const transformScalarFieldValue = (fieldName, value) => {
-  return [fieldName, value];
+const transformScalarFieldValue = ({ name, value }) => {
+  return [name, value];
 }
 
-const transformAssetFieldValue = (fieldName, value, { assets }) => {
+const transformAssetFieldValue = ({ name, value }, { assets }) => {
   if (assets.hasOwnProperty(value)) {
-    return [`${fieldName}___NODE`, assets[value].id];
+    return [`${name}___NODE`, assets[value].id];
   }
-  return [fieldName, null];
+  return [name, null];
 }
 
-const transformImageFieldValue = (fieldName, value, { images }) => {
+const transformImageFieldValue = ({ name, value }, { images }) => {
   if (images.hasOwnProperty(value)) {
-    return [`${fieldName}___NODE`, images[value].id];
+    return [`${name}___NODE`, images[value].id];
   }
-  return [fieldName, null];
+  return [name, null];
 }
 
-const transformGalleryFieldValue = (fieldName, value, { images }) => {
+const transformGalleryFieldValue = ({ name, value }, { images }) => {
   if (Array.isArray(value)) {
     const result = value.map(({ value: imageUrl }) =>
       images.hasOwnProperty(imageUrl)
         ? images[imageUrl].id
         : null
     ).filter(image => image !== null);
-    return [`${fieldName}___NODE`, result];
+    return [`${name}___NODE`, result];
   }
 
   return [fieldName, []];
 }
 
-const transformColorFieldValue = (fieldName, value) => {
+const transformColorFieldValue = ({ name, value }) => {
   const parsedColor = colorString.get.rgb(value);
   const [ red, green, blue, alpha ] = parsedColor;
   const parsedValue = {
@@ -195,39 +126,51 @@ const transformColorFieldValue = (fieldName, value) => {
     hex: colorString.to.hex(parsedColor),
     rgb: colorString.to.rgb(parsedColor),
   };
-  return [fieldName, parsedValue];
+  return [name, parsedValue];
 }
 
-const transformHTMLFieldValue = (fieldName, value) => {
+const transformHTMLFieldValue = ({ name, value }) => {
   // TODO:
   //  - parse HTML Code into react representation
   //  - validate html code and strip stuff we don't want
   //  - replace image URLS
-  return [fieldName, value];
+  return [name, value];
 }
 
-const transformMarkdownFieldValue = (fieldName, value, { markdowns }) => {
-  return [`${fieldName}___NODE`, markdowns[value].id];
+const transformMarkdownFieldValue = ({ name, value }, { markdowns }) => {
+  return [`${name}___NODE`, markdowns[value].id];
 };
 
-const transformLayoutFieldValue = (fieldName, value) => {
+const transformLayoutFieldValue = ({ name, value }) => {
   // TODO:
   //  - create separate node for layout object
   //  - parse data in add-entity-fields.js
-  return [fieldName, JSON.stringify(value)];
+  return [name, JSON.stringify(value)];
 }
 
-const transformObjectFieldValue = (fieldName, value) => {
+const transformObjectFieldValue = ({ name, value }) => {
   // TODO:
   //  - create separate node for json object
   //  - parse data in add-entity-fields.js
-  return [fieldName, JSON.stringify(value)];
+  return [name, JSON.stringify(value)];
 }
 
-const transformUnsupportedTypeFiledValue = (fieldName, value, resources, type) => {
+const transformUnsupportedTypeFiledValue = ({ name, value, type }) => {
   console.warn(`Field type '${type}' is currently not supported by @fika/gatsby-source-cockpit`);
-  return [fieldName, null];
+  return [name, null];
 }
+
+const transformSetFieldValue = ({ name, value }, resources) => {
+  console.log(value);
+  return [name, null];
+}
+
+const transformRepeaterFieldValue = ({ name, value }, resources) => {
+  if (Array.isArray()) {
+    return [name, null];
+  }
+}
+
 /*
 const transformCollectionLinkFieldValue = (fieldName, value) => {
   if (Array.isArray(value)) {
@@ -302,6 +245,9 @@ const valueTransformers = {
   html: transformHTMLFieldValue,
   wysiwyg: transformHTMLFieldValue,
   markdown: transformMarkdownFieldValue,
+
+  // nested fields need to be processed recursively
+
 
   // JSON data is stored stringified (and later stored parsed in a GraphQLJSON field)
   object: transformObjectFieldValue,
