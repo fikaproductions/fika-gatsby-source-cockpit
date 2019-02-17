3c77ac902dbac1a20501da0e6d67db76709a74ae
WhippetsAintDogs <marc.antoine.fortier.94@gmail.com>
2019-02-17 14:13:59 -0500

    chore(Linter): Add prettier to the project

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
-exports.METHODS = { GET: "GET", POST: "POST" };
-exports.MARKDOWN_IMAGE_REGEXP_GLOBAL = /!\[[^\]]*\]\(([^\)]*)\)/g;
-exports.MARKDOWN_ASSET_REGEXP_GLOBAL = /[^!]\[[^\]]*\]\(([^\)]*)\)/g;
-exports.MARKDOWN_IMAGE_REGEXP = /!\[[^\]]*\]\(([^\)]*)\)/;
-exports.MARKDOWN_ASSET_REGEXP = /[^!]\[[^\]]*\]\(([^\)]*)\)/;
+exports.METHODS = { GET: 'GET', POST: 'POST' }
+exports.MARKDOWN_IMAGE_REGEXP_GLOBAL = /!\[[^\]]*\]\(([^\)]*)\)/g
+exports.MARKDOWN_ASSET_REGEXP_GLOBAL = /[^!]\[[^\]]*\]\(([^\)]*)\)/g
+exports.MARKDOWN_IMAGE_REGEXP = /!\[[^\]]*\]\(([^\)]*)\)/
+exports.MARKDOWN_ASSET_REGEXP = /[^!]\[[^\]]*\]\(([^\)]*)\)/
