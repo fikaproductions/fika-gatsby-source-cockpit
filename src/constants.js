2f0f8a24cce6edfbc1011673b4af0600a99bdce6
Felix Bernhardt <felix.bernhardt@mailbox.org>
2020-05-27 13:55:34 +0200

    validate CockpitService parameters

--- src/constants.js
+++ src/constants.js
@@ -5,3 +5,28 @@ exports.MARKDOWN_IMAGE_REGEXP = /!\[[^\]]*\]\(([^\)]*)\)/
 exports.MARKDOWN_ASSET_REGEXP = /[^!]\[[^\]]*\]\(([^\)]*)\)/
 
 exports.TYPE_PREFIX_COCKPIT = 'Cockpit'
+
+function createConfigMessage(message) {
+  return `
+gatsby-source-cockpit: config error
+
+    ${message}
+
+Docs: https://github.com/fikaproductions/fika-gatsby-source-cockpit#how-to-use
+  `
+}
+
+exports.INVALID_BASE_URL = createConfigMessage('"baseUrl" must be a string')
+exports.INVALID_TOKEN = createConfigMessage('"token" must be a string')
+exports.INVALID_LOCALES = createConfigMessage(
+  '"locales" must be a (possibly empty) list of locales'
+)
+exports.INVALID_WHITE_LISTED_COLLECTION_NAMES = createConfigMessage(
+  "'whiteListedCollectionNames' must be a list or undefined"
+)
+exports.INVALID_WHITE_LISTED_SINGLETON_NAMES = createConfigMessage(
+  "'whiteListedSingletonNames' must be a list or undefined"
+)
+exports.INVALID_ALIASES = createConfigMessage(
+  "'aliases' must be an object or undefined"
+)
